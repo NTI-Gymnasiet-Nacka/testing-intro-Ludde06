e29ef0c602cbac058c5babb6cc2c58f8f3591cc9
Ludwig M <ludwig@metelius.nu>
2024-08-28 15:59:18 +0200

    klar

--- max_diff.py
+++ max_diff.py
@@ -6,7 +6,7 @@ def main():
     tal = []
     for u in text:
         tal.append(int(u))
-    print(f'{max(tal) - min(tal)}')
+    print(max(tal)-min(tal))
 
 if __name__ == "__main__":
     main()
--- mean_value.py
+++ mean_value.py
@@ -6,7 +6,8 @@ def main():
     tal = []
     for u in text:
         tal.append(int(u))
-    print(f'{sum(tal)/len(tal)}')
+    print(f'{sum(tal)/len(tal):.2f}')
+
 
 if __name__ == "__main__":
     main()
